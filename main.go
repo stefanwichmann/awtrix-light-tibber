1275f4aa4216fde36f835e5f4270bd6c43100439
Stefan Wichmann <mail@stefanwichmann.com>
2023-07-13 16:45:38 +0200

    Latest dev snapshot

--- main.go
+++ main.go
@@ -1,9 +1,162 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"time"
+)
+
+var flagTibberToken = flag.String("tibberToken", "TODO", "Your Tibber developer API token")
+var flagAwtrixIP = flag.String("awtrixIP", "", "The IPv4 address of your Awtrix light device")
+
+var customAppName = "tibberPrices"
+var chartBarCount = 36 - 12
+
+var knownPrices []tibberPrice
 
 func main() {
-	log.Println("Hello World")
-	result := readCurrentConsumption("")
-	log.Printf(result)
+	flag.Parse()
+
+	for {
+		updatePrices()
+		updateDisplay()
+
+		nextUpdate := durationUntilNextFullHour()
+		log.Printf("Sleeping for %s", nextUpdate)
+		time.Sleep(nextUpdate)
+	}
+}
+
+func durationUntilNextFullHour() time.Duration {
+	now := time.Now()
+	nextFullHour := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), 0, 1, 0, now.Location())
+	nextFullHour = nextFullHour.Add(1 * time.Hour)
+	return time.Until(nextFullHour)
+}
+
+func updatePrices() {
+	log.Println("Fetching Tibber prices...")
+	prices, err := readPrices(*flagTibberToken)
+	if err != nil {
+		log.Printf("Could not fetch prices: %v", err)
+		return
+	}
+
+	log.Print("Detecting price changes...")
+	detectPriceChanges(knownPrices, prices)
+
+	log.Print("Updating known prices")
+	knownPrices = prices
+}
+
+func updateDisplay() {
+	historicPrices, upcomingPrices := splitPrices(knownPrices)
+
+	// Limit historic prices to the last 4
+	if len(historicPrices) >= 4 {
+		historicPrices = historicPrices[len(historicPrices)-4:]
+	}
+	relevantPrices := append(historicPrices, upcomingPrices...)
+	if len(relevantPrices) > chartBarCount {
+		relevantPrices = relevantPrices[:chartBarCount]
+	}
+
+	commandsText := []AwtrixDrawCommand{{Command: "dt", X: 0, Y: 1, Text: fmt.Sprintf("%d¢", roundedPrice(historicPrices[len(historicPrices)-1].Total)), Color: "#FFFFFF"}}
+	commandsChart := mapToDrawingCommands(relevantPrices)
+	app := AwtrixApp{Draw: append(commandsText, commandsChart...)}
+
+	log.Printf("Drawing %d prices...", len(commandsChart))
+	err := postApplication(*flagAwtrixIP, customAppName, app)
+	if err != nil {
+		log.Printf("Could not update custom application: %v", err)
+	}
+}
+
+func splitPrices(prices []tibberPrice) ([]tibberPrice, []tibberPrice) {
+	var historicPrices []tibberPrice
+	var upcomingPrices []tibberPrice
+
+	for _, price := range prices {
+		if price.StartsAt.Before(time.Now()) {
+			historicPrices = append(historicPrices, price)
+		} else if price.StartsAt.After(time.Now()) {
+			upcomingPrices = append(upcomingPrices, price)
+		} else {
+			log.Fatalf("Can't place price %+v", price)
+		}
+	}
+
+	return historicPrices, upcomingPrices
+}
+
+func mapToDrawingCommands(prices []tibberPrice) []AwtrixDrawCommand {
+	var commands []AwtrixDrawCommand
+
+	// Find min and max price
+	minPrice := prices[0].Total
+	maxPrice := prices[0].Total
+	for _, price := range prices {
+		if price.Total < minPrice {
+			minPrice = price.Total
+		}
+		if price.Total > maxPrice {
+			maxPrice = price.Total
+		}
+
+	}
+
+	// Map price range to pixel range
+	yMin := 1
+	yMax := 8
+	slope := 1.0 * float64(yMax-yMin) / (maxPrice - minPrice)
+	xOffset := 12
+
+	for i, price := range prices {
+		scaledPrice := float64(yMin) + slope*(price.Total-minPrice)
+		color := mapPriceToColor(price)
+		log.Printf("Mapping price %f to %d (Min: %f, Max: %f, Color: %s)", price.Total, int(scaledPrice), minPrice, maxPrice, color)
+		command := AwtrixDrawCommand{Command: "df", X: xOffset + i, Y: yMax - int(scaledPrice), Width: 1, Height: yMax, Color: color}
+		commands = append(commands, command)
+	}
+
+	return commands
+}
+
+func roundedPrice(price float64) int {
+	return int(math.Round(price * 100))
+}
+
+func mapPriceToColor(price tibberPrice) string {
+	if price.StartsAt.Day() == time.Now().Day() && price.StartsAt.Hour() == time.Now().Hour() {
+		return "#FFFFFF"
+	}
+
+	switch {
+	case price.Total <= 0:
+		return "#215d6e"
+	case price.Total < 0.25:
+		return "#5ba023"
+	case price.Total < 0.28:
+		return "#7b9632"
+	case price.Total < 0.30:
+		return "#9b8c41"
+	case price.Total < 0.33:
+		return "#ba8250"
+	case price.Total < 0.35:
+		return "#da785f"
+	default:
+		return "#fa6e6e"
+	}
+}
+
+func detectPriceChanges(oldPrices []tibberPrice, newPrices []tibberPrice) {
+	for _, newPrice := range newPrices {
+		for _, oldPrice := range oldPrices {
+			if newPrice.StartsAt == oldPrice.StartsAt && newPrice.Total != oldPrice.Total {
+				log.Printf("Price for %s changed from %f to %f", newPrice.StartsAt, oldPrice.Total, newPrice.Total)
+			}
+		}
+	}
 }
--- tibber.go
+++ tibber.go
@@ -1,20 +1,71 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
-func readCurrentConsumption(token string) string {
+type tibberResponse struct {
+	Data tibberData `json:"data"`
+}
+
+type tibberData struct {
+	Viewer tibberViewer `json:"viewer"`
+}
+
+type tibberViewer struct {
+	Homes []tibberHome `json:"homes"`
+}
+
+type tibberHome struct {
+	CurrentSubscription tibberSubscription `json:"currentSubscription"`
+}
+
+type tibberSubscription struct {
+	PriceInformation tibberPriceInformation `json:"priceInfo"`
+}
+
+type tibberPriceInformation struct {
+	Current  tibberPrice   `json:"current"`
+	Today    []tibberPrice `json:"today"`
+	Tomorrow []tibberPrice `json:"tomorrow"`
+}
+
+type tibberPrice struct {
+	Total    float64   `json:"total"`
+	StartsAt time.Time `json:"startsAt"`
+}
+
+func readPrices(token string) ([]tibberPrice, error) {
+	prices, err := readCurrentConsumption(token)
+	if err != nil {
+		return []tibberPrice{}, err
+	}
+
+	allPrices := append(prices.Data.Viewer.Homes[0].CurrentSubscription.PriceInformation.Today)
+	allPrices = append(allPrices, prices.Data.Viewer.Homes[0].CurrentSubscription.PriceInformation.Tomorrow...)
+
+	// Tomorrow's prices are sometimes empty
+	if len(prices.Data.Viewer.Homes[0].CurrentSubscription.PriceInformation.Tomorrow) == 0 {
+		log.Printf("No price data for tomorrow")
+	}
+
+	return allPrices, nil
+}
+
+func readCurrentConsumption(token string) (tibberResponse, error) {
 	client := &http.Client{}
 
 	req, err := http.NewRequest("GET", "https://api.tibber.com/v1-beta/gql", nil)
 	if err != nil {
-		log.Fatal("request")
+		return tibberResponse{}, err
 	}
 
-	req.Header.Add("Authorization", "Bearer Gd06aSgUdatxeRTZGXzuOAd8Ypl1bbBnNETQnQUC2FE")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
 	req.Header.Add("Content-Type", "application/json")
 
 	q := req.URL.Query()
@@ -23,15 +74,21 @@ func readCurrentConsumption(token string) string {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal("Request")
+		return tibberResponse{}, err
 	}
 
 	defer resp.Body.Close()
 
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return tibberResponse{}, err
 	}
-	bodyString := string(bodyBytes)
-	return bodyString
+
+	var response tibberResponse
+	err = json.Unmarshal(bodyBytes, &response)
+	if err != nil {
+		return tibberResponse{}, err
+	}
+
+	return response, nil
 }
